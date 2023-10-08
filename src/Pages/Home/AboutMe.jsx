9f73e84917ecf3f01c7d0668ed1c08dd1d08e7f5
LoveBhusal <bhusal.love@gmail.com>
2023-10-07 22:34:37 -0500

    sophToJunior

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
           
           <h1 className="skills-section--heading about--heading"style={{color:'#f07900'}}>About Me</h1>
           <p className="hero--section-description about--description" style={{color:'#fff'}}>
-          I am a sophomore at the University of Texas at Dallas majoring in Computer Science. I am passionate and driven in learning new things and applying concepts that I learn into software solutions, whether if it's a side project or a hackathon.
+          I am a junior at the University of Texas at Dallas majoring in Computer Science. I am passionate and driven in learning new things and applying concepts that I learn into software solutions, whether if it's a side project or a hackathon.
           </p>
           <p className="hero--section-description about--description" style={{color:'#fff'}}>
           Outside of school and my major, I have a multitude of hobbies and passions. I love listening to music. My favorite genre is rap/hip-hop and my favorite artists are Kanye West, Lupe Fiasco and Lil Wayne. I find solace in night driving, working out and weightlifting, playing video games, and playing basketball. I am an avid sports enthusiast and I greatly enjoy debating NBA topics with my friends. I also love cooking and watching drama and thriller related movies. My two favorite movies of all time are Scarface and Interstellar.
